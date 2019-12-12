df35bc81f1384ce6fa83496afc59bf67bb566935
bregtje hamer <bregtje.hamer@eijsink.nl>
2019-12-12 15:41:45 +0100

    New version

---
+++ src/android/PM80Scanner.java
@@ -0,0 +1,82 @@
+package com.shineum;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import device.common.DecodeResult;
+import device.common.ScanConst;
+import device.sdk.ScanManager;
+
+// import android.widget.Toast;
+
+public class PM80Scanner extends CordovaPlugin {
+
+  private static ScanManager mScanner = new ScanManager();
+  private static DecodeResult mDecodeResult = new DecodeResult();
+  private static CallbackContext callbackContext = null;
+
+  static {
+    mScanner.aDecodeSetResultType(ScanConst.ResultType.DCD_RESULT_USERMSG);
+    mScanner.aDecodeSetTriggerMode(ScanConst.TriggerMode.DCD_TRIGGER_MODE_ONESHOT);
+    // mScanner.aDecodeSetBeepEnable(0);   // 1 : on, 0 : off
+  }
+
+  public static class ScanResultReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+      // Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+			if (mScanner != null) {
+				mScanner.aDecodeGetResult(mDecodeResult.recycle());
+        if (callbackContext != null) {
+          if (mDecodeResult.symName.equals("READ_FAIL")) {
+              callbackContext.error("READ_FAIL");
+          } else {
+            JSONObject json = new JSONObject();
+            try {
+              json.put("result", mDecodeResult.toString());
+              json.put("type", mDecodeResult.symName);
+            } catch (Exception e) {}
+            callbackContext.success(json);
+          }
+        }
+			} else {
+          if (callbackContext != null) {
+              callbackContext.error("SCANNER_ERROR");
+          }
+      }
+		}
+	}
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+      if (action.equals("scan")) {
+        this.callbackContext = callbackContext;
+        mScanner.aDecodeSetTriggerOn(1);
+      } else if (action.equals("cancel")) {
+        if (this.callbackContext != null) {
+          this.callbackContext.error("USER_CANCEL");
+          this.callbackContext = null;
+        }
+        mScanner.aDecodeSetTriggerOn(0);
+      } else if (action.equals("beep")) {
+        mScanner.aDecodeSetBeepEnable(args.getInt(0));
+      } else if (action.equals("set")) {
+        this.callbackContext = callbackContext;
+      } else if (action.equals("triggerMode")) {
+        mScanner.aDecodeSetTriggerMode(args.getInt(0));
+      } else if (action.equals("scan2")) {
+        mScanner.aDecodeSetTriggerOn(1);
+      } else {
+        return false;
+      }
+      return true;
+  }
+}
---
+++ www/pm80scanner.js
@@ -0,0 +1,74 @@
+var exec = cordova.require("cordova/exec");
+
+/**
+ * Constructor.
+ *
+ * @returns {Pm80Scanner}
+ */
+function Pm80Scanner() {
+}
+
+/**
+ * Read code from scanner.
+ *
+ * @param {Function} successCallback
+ * @param {Function} errorCallback
+ * @param config
+ */
+Pm80Scanner.prototype.scan = function (successCallback, errorCallback, config) {
+      if (successCallback != null) {
+          pm80Scanner.set(successCallback, errorCallback);
+      }
+      exec(null, null, 'PM80Scanner', "scan2", null);
+};
+
+/**
+ * Cancel.
+ */
+Pm80Scanner.prototype.cancel = function () {
+      exec(null, null, 'PM80Scanner', 'cancel', null);
+};
+
+/**
+ * Beep.
+ */
+Pm80Scanner.prototype.beep = function (mode) {
+      exec(null, null, 'PM80Scanner', 'beep', [mode ? 1 : 0]);
+};
+
+/**
+ * Register Callback.
+ */
+Pm80Scanner.prototype.set = function (successCallback, errorCallback) {
+      exec(
+        function(resultObj) {
+            try {
+              successCallback(resultObj.result, resultObj);
+            } catch (err) {}
+            pm80Scanner.set(successCallback, errorCallback);
+        }
+        , function(error) {
+            try {
+              errorCallback(error);
+            } catch (err) {}
+            pm80Scanner.set(successCallback, errorCallback);
+        }, 'PM80Scanner', 'set', null);
+};
+
+/**
+ * Set Trigger Mode.
+ */
+Pm80Scanner.prototype.setTriggerMode = function (mode) {
+      if (mode == pm80Scanner.TRIGGER_MODE.ONESHOT || mode == pm80Scanner.TRIGGER_MODE.AUTO || mode == pm80Scanner.TRIGGER_MODE.CONTINUOUS) {
+          exec(null, null, 'PM80Scanner', 'triggerMode', [mode]);
+      }
+};
+
+Pm80Scanner.prototype.TRIGGER_MODE = {
+    ONESHOT            : 0
+    , AUTO             : 1
+    , CONTINUOUS       : 2
+};
+
+var pm80Scanner = new Pm80Scanner();
+module.exports = pm80Scanner;
